8b9122864f1aa011a2bd36ee7029215741064a25
Ani Argjiri <aargjiri@akamai.com>
2025-04-05 22:04:22 +0200

    feat: major rework BREAKING CHANGE: Major rework

--- index.js
+++ index.js
@@ -10,40 +10,5 @@ app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
-// a new fix
-// more fixes
-
-// a feature
-// new feature
-// another feature
-// add fix
-// BREAKING CHANGE: fix
-//fix number 1
-//fix number 2
-//fix number 3
-//fix number 4
-//fix number 5
-//fix number 6
-//fix number 7
-//fix number 8
-//fix number 9
-//fix number 10
-//feat number 1
-//feat number 2
-//feat number 3
-//feat number 4
-//feat number 5
-//feat number 6
-//feat number 7
-//feat number 8
-//feat number 9
-//feat number 10
-// BREAKING CHANGE: feat
-// a fix
-// another fix
-// a fix
-
-//feat: new feature
-//breaking change
-
-// chore: add a chore
+// 
+// BREAKING CHANGE: Major rework
